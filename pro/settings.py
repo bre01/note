c9b3deaab190ffebc636cd7a2f61cb9539fa8fdc
bre <asset0730@outlook.com>
2022-11-22 18:41:23 +0800

    success

--- pro/settings.py
+++ pro/settings.py
@@ -34,7 +34,7 @@ INSTALLED_APPS = [
     #my apps
     'app_note',
     #third party
-    'bootstrap4',
+    'bootstrap5',
 
     'django.contrib.admin',
     'django.contrib.auth',
@@ -120,8 +120,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
-STATIC_URL = 'static/'
+STATIC_URL = ''
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
