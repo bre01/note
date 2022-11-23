a81ba7949ae7e77873c45b0ce696650476dcce12
bre <asset0730@outlook.com>
2022-11-23 18:03:45 +0800

    use bootstrap

--- app_note/models.py
+++ app_note/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Topic(models.Model):#inheritance the class Models.Model 
     topic_name=models.CharField(max_length=200)
-    date_added=models.DateField(auto_now_add=True)
+    date_added=models.DateTimeField(auto_now_add=True)
     def __str__(self): # django use __str__() method as default way to show the basic info
         return self.topic_name
 
@@ -17,10 +17,11 @@ class Description(models.Model):
 
 class Entry(models.Model):
     topic=models.ForeignKey(Topic, on_delete=models.CASCADE) # connetct entry to topic by a "id" key
-    text=models.TextField()
+    title=models.TextField(max_length=50)
+    text=models.TextField(max_length=10000,default="why")
     date_added=models.DateTimeField(auto_now_add=True)
     class Meta():
         verbose_name_plural="entries"
     def __str__(self):
-        return self.text[0:20]+"..."
+        return self.title
 
